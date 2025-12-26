c49de88c03bf8f4abdd8d23cc6d2e1c61185ed99
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-12-26 12:37:19 +0530

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,10 +21,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-        if (app()->environment('production')) {
-        URL::forceScheme('https');
     }
-    }
-
-    
 }
