b0474e9db13102f0c20ffef49f8dbde8618496f7
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-12-26 17:36:48 +0530

    fix

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -266,20 +266,23 @@ public function store(Request $request)
         ]);
 
     } catch (\Throwable $e) {
-        DB::rollBack();
+    DB::rollBack();
 
-        Log::error('REGISTER: Exception', [
-            'message' => $e->getMessage(),
-            'line' => $e->getLine(),
-            'file' => $e->getFile(),
-        ]);
+    Log::error('REGISTER: Exception', [
+        'message' => $e->getMessage(),
+        'line' => $e->getLine(),
+        'file' => $e->getFile(),
+    ]);
 
-        return response()->json([
-            'errors' => [
-                'general' => ['Something went wrong. Please contact admin.']
-            ]
-        ], 422);
-    }
+    return response()->json([
+        'errors' => [
+            'general' => [
+                'Something went wrong. Please contact admin.',
+                'Error: '.$e->getMessage().' ('.$e->getFile().':'.$e->getLine().')',
+            ],
+        ],
+    ], 422);
+}
 }
     /**
      * Resend OTP
